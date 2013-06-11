bc5c3b0a957fb625ddf7902f82d199edd56b5643
David Dobervich <david.dobervich@gmail.com>
2013-06-11 13:53:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wombat::Application.config.secret_token = '5a9623374607d7377cf09620abde03dd8bf40576b7b876f9752895d26d3a137da96810bff25334d8f858d396d7316d78671375b7db9cf3f7e0bc6bfad2f4440f'
